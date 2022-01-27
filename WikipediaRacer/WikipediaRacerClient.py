9e08348e02d6d82752c5f57cc9d37d24a4b759fc
mfpankau <74994673+mfpankau@users.noreply.github.com>
2022-01-27 17:37:46 -0500

    Update WikipediaRacerClient.py

--- WikipediaRacer/WikipediaRacerClient.py
+++ WikipediaRacer/WikipediaRacerClient.py
@@ -1,10 +1,46 @@
 from flask import Flask, jsonify, request
-import time
+from matplotlib.cbook import print_cycles
+import requests
 
 app = Flask(__name__)
 
 hostIP = ''
+myUser = ''
+
+
+@app.route('/playerName', methods=['POST'])
+def setPlayer():
+    global myUser
+    if myUser == '':
+        #params = request.get_json()
+        myUser = request.args.get('player')
+    else:
+        print('Warning, username already set!')
+        #params = request.get_json()
+        myUser = request.args.get('player')
+    print('new username set to', myUser)
+    return 'got name'
+
+
+@app.route('/setHostIP', methods=['POST'])
+def setHostIP():
+    global hostIP
+    if hostIP == '':
+        #params = request.get_json()
+        hostIP = request.args.get('ip')
+        r = requests.post('http://' + hostIP + '/addPlayer', params={'player': myUser})
+        if r.status_code == 200:
+            print('connected to ', hostIP)
+        return 'got IP'
+    else: print('Warning, already in a lobby!')
+
 
 @app.route('/acceptUrl', methods=['POST'])
 def acceptUrl():
-    pass
+    #params = request.get_json()
+    url = request.args.get('url')
+    print('Recieved url:', url, ' sending to host')
+    r = requests.post('http://' + hostIP + '/sendUrl', params={'url': url, 'player': myUser})
+    if r.status_code == 200:
+        print('yea it worked pog')
+    return ''
